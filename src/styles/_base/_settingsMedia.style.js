504e3f8911ec59125912fe4c990fa80ba41c0c16
Samuel Hung <samuelhung@Samuels-MacBook-Pro.local>
2022-07-15 20:13:04 +0200

    Added CDK for deployment and ran first build.

--- src/styles/_base/_settingsMedia.style.js
+++
@@ -1,6 +0,0 @@
-export const mediaSizes = {
-    phone: '600px',
-    tablet: '768px',
-    desktop: '992px',
-    lrgDesktop: '1200px',
-}
--- src/styles/_layout/_layout.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const LayoutStyle = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-`
