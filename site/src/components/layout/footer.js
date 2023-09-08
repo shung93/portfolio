376bb213c2eda399eef6e953de458850d574f3be
Yuji Nelson <k.yuji.nelson@gmail.com>
2023-09-08 12:29:41 +0100

    feat(footer): updates hard-coded year to current year

--- site/src/components/layout/footer.js
+++ site/src/components/layout/footer.js
@@ -1,31 +1,19 @@
-import {Link} from 'gatsby';
-import React from 'react';
-import { FooterStyle } from '../../styles/_layout/_footer.style';
+import { Link } from "gatsby";
+import React from "react";
+import { FooterStyle } from "../../styles/_layout/_footer.style";
 
 const Footer = () => {
-    return (
-        <div>
-            <FooterStyle
-                  className="dark-mode"
-            >
-                <div
-                    className="links"
-                >
-                    <Link
-                        to="https://www.linkedin.com/in/samhung/"
-                    >
-                        linkedin
-                    </Link>
-                    <Link
-                        to="https://github.com/shung93"
-                    >
-                        github
-                    </Link>
-                </div>
-                <p>© 2022</p>
-            </FooterStyle>
+  return (
+    <div>
+      <FooterStyle className="dark-mode">
+        <div className="links">
+          <Link to="https://www.linkedin.com/in/samhung/">linkedin</Link>
+          <Link to="https://github.com/shung93">github</Link>
         </div>
-    )
+        <p>© {new Date().getFullYear()}</p>
+      </FooterStyle>
+    </div>
+  );
 };
 
 export default Footer;
